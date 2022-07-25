8114d993de400e02f509e81a4e0660cdab1efb55
brendontwj <bren0019@e.ntu.edu.sg>
2022-07-25 10:43:09 +0800

    readme

--- README.md
+++ README.md
@@ -2,22 +2,21 @@
 
 * Run the springboot app
 
----
-mvn spring-boot:run -Dspring-boot.run.arguments=--dataDir=C:\Users\Brendon\JavaProjects\ssfworkshop3
----
+
+> mvn spring-boot:run -Dspring-boot.run.arguments=--dataDir=C:\Users\Brendon\JavaProjects\ssfworkshop3
+
 
 * Incorporate test dependencies
 
----
+```
 <dependency>
     <groupId>org.springframework.boot</groupId>
     <artifactId>spring-boot-starter-test</artifactId>
     <version>2.7.1</version>
     <scope>test</scope>
 <dependency>
----
+```
 
 * In order to execute test cases
----
-mvn test
----
+
+> mvn test
--- src/main/java/com/vttp2022/ssfworkshop3/util/Contacts.java
+++ src/main/java/com/vttp2022/ssfworkshop3/util/Contacts.java
@@ -1,15 +1,21 @@
 package com.vttp2022.ssfworkshop3.util;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.vttp2022.ssfworkshop3.model.Contact;
 
@@ -38,5 +44,31 @@ public class Contacts {
             printwriter.close();
         }
         model.addAttribute("contact", new Contact(ctc.getId(), ctc.getName(), ctc.getEmail(), ctc.getPhoneNumber()));
+    }
+
+    public void getContactById(Model model, String contactId, ApplicationArguments appArgs) {
+        Contact ctc = new Contact();
+        try {
+            Path filePath = new File(getDataDir(appArgs) + "/" + contactId).toPath();
+            Charset charset = Charset.forName("UTF-8");
+            List<String> stringList = Files.readAllLines(filePath, charset);
+            ctc.setId(contactId);
+            ctc.setName(stringList.get(0));
+            ctc.setEmail(stringList.get(1));
+            ctc.setPhoneNumber(Integer.parseInt(stringList.get(2)));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact info not found.");
+        }
+
+        model.addAttribute("contact", ctc);
+    }
+
+    private String getDataDir(ApplicationArguments appArgs) {
+        Set<String> opsNames = appArgs.getOptionNames();
+        String[] optNamesArr = opsNames.toArray(new String[opsNames.size()]);
+        List<String> optValues = appArgs.getOptionValues(optNamesArr[0]);
+        String[] optValuesArr = optValues.toArray(new String[optValues.size()]);
+        return optValuesArr[0];
     }
 }
